103cda3704100add93840ee42933ea3a118d2eae
Matt Cassell <matt.cassell@tierpoint.com>
2022-11-25 11:30:39 -0600

    Update tierpoint.sh

--- tierpoint.sh
+++ tierpoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+INSTANCE_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
 CREDS=$(pwmake 4)
 FILE=/home/ec2-user/.ssh/.../delete_me.txt
 
@@ -9,3 +10,4 @@ fi
 
 aws dynamodb put-item --table-name linux-challenge --item '{"password": {"S":"'"$CREDS"'"}}' --region us-east-1
 echo $CREDS | passwd --stdin tieradmin
+echo "sh aws ec2 terminate-instances --instance-ids '"$INSTANCE_ID"'" | at 'now + 30 minutes'
