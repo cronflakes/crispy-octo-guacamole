36e37f3e5651f4dc9e61af8a0542748f2621a97d
Matt Cassell <matt.cassell@tierpoint.com>
2022-11-25 14:09:08 -0600

    adding date stamp for getting latest entry in lambda

--- tierpoint.sh
+++ tierpoint.sh
@@ -2,12 +2,13 @@
 INSTANCE_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
 CREDS=$(pwmake 4)
 FILE=/home/ec2-user/.ssh/.../delete_me.txt
+DATESTAMP=$(date +"%s")
 
 if [[ ! -f $FILE ]]; then
         fallocate -l 6G $FILE
 fi
 
-aws dynamodb put-item --table-name linux-challenge --item '{"password": {"S":"'"$CREDS"'"}}' --region us-east-1
+aws dynamodb put-item --table-name linux-challenge --item '{"password": {"S":"'"$CREDS"'"}, "date": {"S":"'"$DATESTAMP"'"}}' --region us-east-1
 echo $CREDS | passwd --stdin tieradmin
 echo "sh aws ec2 terminate-instances --instance-ids '"$INSTANCE_ID"'" | at 'now + 30 minutes'
 
